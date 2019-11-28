72b1d4e059de44ff21a0153a34c288fb57ddfe98
Moulue Huang <31867712+MouHuang0217@users.noreply.github.com>
2019-11-27 18:53:12 -0800

    Update SalesPerson.java

--- src/main/SalesPerson.java
+++ src/main/SalesPerson.java
@@ -9,6 +9,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author mou lue huang
@@ -27,19 +29,82 @@ public final class SalesPerson {
     }
     public SalesPerson(){
     }
+    
     //Use Case 14
     //display totalcomission
-    private void displayTotalCommission(String name){
+    void displayTotalCommission(String firstName, String lastName){
+        Statement stmt = null;
+        ResultSet rs = null;
         //query to check for the name
+        boolean valid = checkSalesPerson(firstName,lastName);
+        String salesPerson_display_format = "%-25s%-25s%-25s%-25s\n";
+        System.out.printf(salesPerson_display_format, "firstName", "lastName","commission rate","totalcommission");
+        try{
+            if (valid){
+                stmt = conn.createStatement();
+                String salesPersonSQL = "SELECT FIRSTNAME, LASTNAME, COMMISSIONRATE,TOTALCOMMISSION FROM EMPLOYEES";
+                rs = stmt.executeQuery(salesPersonSQL);
+                
+                while (rs.next()){
+                    String first_name = rs.getString("FIRSTNAME");
+                    String last_name = rs.getString("LASTNAME");
+                    int commissionrate = rs.getInt("COMMISSIONRATE");
+                    int totalcommission = rs.getInt("TOTALCOMMISSION");
+                    System.out.printf(salesPerson_display_format, dispNull(firstName), dispNull(lastName),commissionrate/100 + "%","$"+totalcommission);
+                }
+                rs.close();
+                stmt.close();
+            }
+            else{
+                System.out.println("No valid sales person in the database");
+            }
+        }
+        catch (SQLException sqlExcept)
+        {
+            sqlExcept.printStackTrace();
+        }
         
     }
     //Use Case 9
     //display total Sales
-    private void displayTotalSales(String name){
+    private void displayTotalSales(String firstName, String lastName){
         //query to check for the name
         
     }
-    
+    boolean checkSalesPerson(String i_firstName, String i_lastName){
+        Statement stmt = null;
+        ResultSet rs = null;
+        System.out.println(i_firstName);
+        
+        System.out.println(i_lastName);
+        try {
+            stmt = conn.createStatement();
+            String salesPersonSQL = "SELECT * FROM EMPLOYEES";
+            //initlize for salesperson flag
+            boolean s_name_flag = false;
+            rs = stmt.executeQuery(salesPersonSQL);
+            
+            //loop through database and see if the user input's salesperson's name exists or not
+            //if exists, set the flag to be true
+            while (rs.next()) {
+                String first_name = rs.getString("FIRSTNAME");
+                String last_name = rs.getString("LASTNAME");
+                //i_first/lastname is the name we can return true
+                if (i_firstName.equals(first_name) && i_lastName.equals(last_name)) {
+                    s_name_flag  = true;
+                }
+            }
+            rs.close();
+            stmt.close();
+            return s_name_flag;
+            
+        } 
+        catch (SQLException sqlExcept)
+        {
+            sqlExcept.printStackTrace();
+        }
+        return false;
+    }
     
     //display all Sales person
     void displayAllSalesPerson(){
@@ -51,7 +116,9 @@ public final class SalesPerson {
             ResultSet rs = null;
             rs = stmt.executeQuery(getMaxSQL);
             
-            String salesPerson_display_format = "%-25s%-25s%-25s%-25s%-25s%-25s%-25s\n";
+            String salesPerson_display_format = "%-25s%-25s%-25s%-25s%-25s%-25s%-25s%-25s\n";
+            System.out.printf(salesPerson_display_format, "employeeID", "firstName", "lastName", "phoneNum", "address","salary","commissionrate","totalcommission");
+
             while (rs.next()) {
                 int employeeID = rs.getInt("EID");
                 String firstName = rs.getString("FIRSTNAME");
@@ -60,14 +127,18 @@ public final class SalesPerson {
                 String address = rs.getString("ADDRESS");
                 int salary = rs.getInt("SALARY");
                 int commissionrate = rs.getInt("COMMISSIONRATE");
-                System.out.printf(salesPerson_display_format, employeeID, dispNull(firstName), dispNull(lastName), dispNull(phoneNum), dispNull(address),salary,commissionrate/100);
+                int totalcommission = rs.getInt("TOTALCOMMISSION");
+                System.out.printf(salesPerson_display_format, employeeID, dispNull(firstName), dispNull(lastName), dispNull(phoneNum), dispNull(address),salary,commissionrate/100,totalcommission);
                             
             }
+            rs.close();
+            stmt.close();
         }
         catch(SQLException sqlExcept)
         {
             sqlExcept.printStackTrace();
         }
+    
     }
     
 
@@ -77,7 +148,7 @@ public final class SalesPerson {
         try
         {
             stmt = conn.createStatement();
-            String insertNewSPSQL = String.format("INSERT INTO Employees(EID,FIRSTNAME,LASTNAME,PHONENUMBER,ADDRESS,SALARY,COMMISSIONRATE) values (%d,'%s','%s','%s','%s',%d,%d)", eID,firstName,lastName,phoneNum,address,salary,commission);
+            String insertNewSPSQL = String.format("INSERT INTO Employees(EID,FIRSTNAME,LASTNAME,PHONENUMBER,ADDRESS,SALARY,COMMISSIONRATE,TOTALCOMMISSION) values (%d,'%s','%s','%s','%s',%d,%d,%d)", eID,firstName,lastName,phoneNum,address,salary,commission,0);
             System.out.println(insertNewSPSQL);
             stmt.executeUpdate(insertNewSPSQL);
         }
