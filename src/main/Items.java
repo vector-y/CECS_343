2cdbffcfe598528c123622f42d2213ea6e2ae2c1
Joshasup <hasupsong@gmail.com>
2019-12-03 22:48:49 -0800

    Update Items.java
    Updated Item class

--- src/main/Items.java
+++ src/main/Items.java
@@ -1,8 +1,12 @@
 package main;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class Items {
@@ -10,7 +14,7 @@ public class Items {
     Connection conn = dbConnection.connect();
     
     private String name;
-    private float quantity;
+    private int quantity;
     private float sellingPrice;
     private float costPrice;
     private int quantitySold;
@@ -18,7 +22,7 @@ public class Items {
     private float profit;
     
     
-    public Items(String itemName, float itemQuantity, float itemSellingPrice, float itemCostPrice) {
+    public Items(String itemName, int itemQuantity, float itemSellingPrice, float itemCostPrice) {
     	
     	name = itemName;
     	quantity = itemQuantity;
@@ -27,29 +31,54 @@ public class Items {
     	quantitySold = 0;
     	priceSold = 0;
     	profit = 0;
-    	insertSPToDatabase();
+    	
     }
     
     public Items() {
     
     }
     
-    public void addQuantity(float number)
+    public void addQuantity(int productID, int number)
     {
+    	Statement stmt = null;
+        ResultSet rs = null;
+        
+        stmt = conn.createStatement();
+        String productSQL = "SELECT QUANTITYINSTOCK FROM PRODUCTS WHERE EID =" + productID;
+        rs = stmt.executeQuery(productSQL);
+        quantity = rs.getInt("QUANTITYINSTOCK");
     	quantity = quantity + number;
+    	conn.close();
+    	String query = "UPDATE PRODUCTS SET QUANTITYINSTOCK = ? WHERE EID =" + productID;
+    	PreparedStatement proSql = conn.prepareStatement(query);
+    	proSql.setInt(1, quantity);
+    	proSql.executeUpdate();
+    	conn.close();
     }
     
-    public void changeQuantity(float number)
+    public void changeQuantity(int productID, int number)
     {
+    	Statement stmt = null;
+        ResultSet rs = null;
+        
+        stmt = conn.createStatement();
+        String productSQL = "SELECT QUANTITYINSTOCK FROM PRODUCTS WHERE EID =" + productID;
+        rs = stmt.executeQuery(productSQL);
     	quantity = number;
+    	conn.close();
+    	String query = "UPDATE PRODUCTS SET QUANTITYINSTOCK = ? WHERE EID =" + productID;
+    	PreparedStatement proSql = conn.prepareStatement(query);
+    	proSql.setInt(1, quantity);
+    	proSql.executeUpdate();
+    	conn.close();
     }
     
-    public float getQuantity()
+    public float getQuantity(int productID)
     {
     	return quantity;
     }
     
-    public void addQuantitySold(int number)
+    public void addQuantitySold(int productID, int number)
     {
     	quantitySold = quantitySold + number;
     }
@@ -109,13 +138,18 @@ public class Items {
     	return profit;
     }
     
+    public void displayProfitList()
+    {
+    	
+    	
+    }
     
     void insertSPToDatabase() {
         Statement stmt;
         try
         {
             stmt = conn.createStatement();
-            String insertNewSPSQL = String.format("INSERT INTO Items(NAME,ITEMQUANTITY,MSRP,COSTPRICE, QUANTITYSOLD, PRICESOLD) values (%s,'%d','%d','%d','%d','%d')",name, quantity, sellingPrice, costPrice, quantitySold, priceSold);
+            String insertNewSPSQL = String.format("INSERT INTO products(productName,quantityInStock,MSRP,buyPrice, QUANTITYSOLD, PRICESOLD) values (%s,'%d','%d','%d','%d','%d')",name, quantity, sellingPrice, costPrice, quantitySold, priceSold);
             System.out.println(insertNewSPSQL);
             stmt.executeUpdate(insertNewSPSQL);
         }
